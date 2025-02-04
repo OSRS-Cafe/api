63ab5f5ed0d52d67e6671aa3d7f206cfccfcd9a6
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-02-04 21:43:54 +0100

    Add BadHiscoreModeException

--- src/main/kotlin/cafe/osrs/api/utils/APIException.kt
+++ src/main/kotlin/cafe/osrs/api/utils/APIException.kt
@@ -1,5 +1,6 @@
 package cafe.osrs.api.utils
 
+import cafe.osrs.api.clients.hiscore.HiscoreMode
 import io.ktor.http.*
 
 open class RSApiException(
@@ -7,5 +8,6 @@ open class RSApiException(
     val status: HttpStatusCode
 ): Exception(message)
 
+class BadHiscoreModeException : RSApiException("Bad Mode Format! Available: ${HiscoreMode.prettyList}", HttpStatusCode.BadRequest)
 class NameTooLongException(name: String): RSApiException("Player name ($name) cant exceed 12 characters", HttpStatusCode.BadRequest)
 class NameEmptyException: RSApiException("Player name cant be empty", HttpStatusCode.BadRequest)
