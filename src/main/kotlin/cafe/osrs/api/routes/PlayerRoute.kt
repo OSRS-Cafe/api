fb63dd99b98014ff323cb1b414539772d4f83220
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-02-05 18:34:18 +0100

    Add Combat Level to Player Route + Ranked Query Parameter

--- src/main/kotlin/cafe/osrs/api/routes/PlayerRoute.kt
+++ src/main/kotlin/cafe/osrs/api/routes/PlayerRoute.kt
@@ -2,28 +2,47 @@ package cafe.osrs.api.routes
 
 import cafe.osrs.api.clients.hiscore.HiscoreClient
 import cafe.osrs.api.clients.hiscore.HiscoreMode
+import cafe.osrs.api.clients.hiscore.HiscoreResponseActivtityDTO
+import cafe.osrs.api.clients.hiscore.HiscoreResponseSkillDTO
 import cafe.osrs.api.utils.*
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlayerInfoResponse(
+    val combatLevel: Double,
+    val skills: List<HiscoreResponseSkillDTO>,
+    val activities: List<HiscoreResponseActivtityDTO>
+)
 
 fun Route.PlayerRoute() {
-    //TODO: Add combat level
     get("/player/{mode}/{name}") {
         val mode = HiscoreMode.get(call.pathParameters["mode"]) ?: throw BadHiscoreModeException()
         val name = call.pathParameters["name"].verifyValidCharacterName()
+        val rankedOnly = call.queryParameters.contains("ranked")
 
         val response = HiscoreClient.getHiscore(mode = mode, player = name)
-        setHiscoreResponseHeaders(response)
+        call.setHiscoreResponseHeaders(response)
 
         when(response.status) {
-            HttpStatusCode.OK, HttpStatusCode.NotFound -> call.respondText(status = response.status, text = response.response ?: "")
+            HttpStatusCode.OK -> {
+                val data = response.response ?: throw GenericRSApiException(message = "Hiscore Response Status is OK but response is null")
+                val info = PlayerInfoResponse(
+                    combatLevel = CombatLevelCalculator.calculate(data),
+                    skills = data.skills.filter { !rankedOnly || it.rank != -1 },
+                    activities = data.activities.filter { !rankedOnly || it.rank != -1 }
+                )
+                call.respond(status = HttpStatusCode.OK, message = info)
+            }
+            HttpStatusCode.NotFound -> call.respondText(status = HttpStatusCode.NotFound, text = "")
             else -> {
                 val errorId = String.random(10)
                 println("[$errorId] Error!")
                 println("[$errorId] Unexpected status: ${response.status}")
                 println("[$errorId] Response: $response")
-                call.respondText(status = HttpStatusCode.InternalServerError, text = "Internal Server Error. Error id: $errorId")
+                throw GenericRSApiException(message = "Encountered an unexpected response status! Error id: $errorId")
             }
         }
     }
