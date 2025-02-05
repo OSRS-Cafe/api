bb6b351dc6a2c55581562ef7b54bad4183e6bec0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-02-05 17:18:50 +0100

    Make setHiscoreResponseHeaders to extend ApplicationCall

--- src/main/kotlin/cafe/osrs/api/utils/Extensions.kt
+++ src/main/kotlin/cafe/osrs/api/utils/Extensions.kt
@@ -4,6 +4,7 @@ import cafe.osrs.api.APIConfig
 import cafe.osrs.api.clients.hiscore.HiscoreResponse
 import io.ktor.client.*
 import io.ktor.client.plugins.*
+import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -26,14 +27,14 @@ fun HttpClientConfig<*>.addUserAgent() {
 
 fun getUnixTime() = System.currentTimeMillis() / 1000
 
-fun RoutingContext.setHiscoreResponseHeaders(response: HiscoreResponse) {
+fun ApplicationCall.setHiscoreResponseHeaders(response: HiscoreResponse) {
     //TODO: Put this into the response json?
     //Later sven: Probably not, this way its very convenient to pass this message for multiple kind of responses
     //We should however make sure to have different cache headers for different kinds of clients (eg: Hiscore vs GE)
-    call.response.header("RS-Cache-QueryTime", response.queryTime)
-    call.response.header("RS-Cache-Cached", response.cached.toString())
-    call.response.header("RS-Cache-Time-Until-Refresh", response.timeUntilRefresh)
-    call.response.header("content-type", "application/json")
+    this.response.header("RS-Cache-QueryTime", response.queryTime)
+    this.response.header("RS-Cache-Cached", response.cached.toString())
+    this.response.header("RS-Cache-Time-Until-Refresh", response.timeUntilRefresh)
+    this.response.header("content-type", "application/json")
 }
 
 fun String?.verifyValidCharacterName(): String {
