49343cc47ba0548c16699187f92d075ad8fd7ac5
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-02-09 14:51:21 +0100

    Create Config Option "GE-CACHE-TIME-MINUTES"

--- src/main/kotlin/cafe/osrs/api/APIConfig.kt
+++ src/main/kotlin/cafe/osrs/api/APIConfig.kt
@@ -10,6 +10,7 @@ object APIConfig {
     val tokenBucketRefill: Int = env(name = "API-TOKEN-BUCKET-REFILL", default = 60)
     val userAgent: String = env(name = "API-USER-AGENT")
     val hiscoreCacheTimeMinutes: Int = env(name = "HS-CACHE-TIME-MINUTES", default = 10)
+    val geCacheTimeMinutes: Int = env(name = "GE-CACHE-TIME-MINUTES", default = 1)
 
     private inline fun <reified T> env(name: String, default: T? = null): T {
         val envValue = System.getenv(name)
