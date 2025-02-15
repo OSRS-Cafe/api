8e8cced3fc7836b5b53d57712a3c600c1e69b4a7
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-02-15 15:31:51 +0100

    Add volume path to API Config

--- src/main/kotlin/cafe/osrs/api/APIConfig.kt
+++ src/main/kotlin/cafe/osrs/api/APIConfig.kt
@@ -11,8 +11,10 @@ object APIConfig {
     val userAgent: String = env(name = "API-USER-AGENT")
     val hiscoreCacheTimeMinutes: Int = env(name = "HS-CACHE-TIME-MINUTES", default = 10)
     val geCacheTimeMinutes: Int = env(name = "GE-CACHE-TIME-MINUTES", default = 1)
+    val volumePath: String = env(name = "API-VOLUME-PATH")
 
     private inline fun <reified T> env(name: String, default: T? = null): T {
+        //TODO: Allow enums here
         val envValue = System.getenv(name)
         if(envValue != null) return castTo(envValue)
         if(default != null) return default
