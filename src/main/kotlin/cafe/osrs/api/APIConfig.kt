e15b390bd9ee96073a0580a72ec080b8638e144e
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-03-30 20:10:59 +0200

    APIConfig: Use Underscores for env names

--- src/main/kotlin/cafe/osrs/api/APIConfig.kt
+++ src/main/kotlin/cafe/osrs/api/APIConfig.kt
@@ -3,17 +3,17 @@ package cafe.osrs.api
 import kotlin.reflect.typeOf
 
 object APIConfig {
-    val host: String = env(name = "API-HOST", default = "0.0.0.0")
-    val port: Int = env(name = "API-PORT", default = 8080)
-    val tokenWeight: Int = env(name = "API-TOKEN-WEIGHT", default = 100)
-    val tokenBucket: Int = env(name = "API-TOKEN-BUCKET", default = 6_000)
-    val tokenBucketRefill: Int = env(name = "API-TOKEN-BUCKET-REFILL", default = 60)
-    val userAgent: String = env(name = "API-USER-AGENT")
-    val hiscoreCacheTimeMinutes: Int = env(name = "HS-CACHE-TIME-MINUTES", default = 10)
-    val geCacheTimeMinutes: Int = env(name = "GE-CACHE-TIME-MINUTES", default = 1)
-    val playerCountCacheTimeSeconds: Int = env(name = "PLAYER-COUNT-CACHE-SECONDS", default = 30)
-    val volumePath: String = env(name = "API-VOLUME-PATH")
-    val worldsListCacheTimeMinutes: Int = env(name = "WORLDS-CACHE-TIME-MINUTES", default = 5)
+    val host: String = env(name = "API_HOST", default = "0.0.0.0")
+    val port: Int = env(name = "API_PORT", default = 8080)
+    val tokenWeight: Int = env(name = "API_TOKEN_WEIGHT", default = 100)
+    val tokenBucket: Int = env(name = "API_TOKEN_BUCKET", default = 6_000)
+    val tokenBucketRefill: Int = env(name = "API_TOKEN_BUCKET_REFILL", default = 60)
+    val userAgent: String = env(name = "API_USER_AGENT")
+    val hiscoreCacheTimeMinutes: Int = env(name = "HS_CACHE_TIME_MINUTES", default = 10)
+    val geCacheTimeMinutes: Int = env(name = "GE_CACHE_TIME_MINUTES", default = 1)
+    val playerCountCacheTimeSeconds: Int = env(name = "PLAYER_COUNT_CACHE_SECONDS", default = 30)
+    val volumePath: String = env(name = "API_VOLUME_PATH")
+    val worldsListCacheTimeMinutes: Int = env(name = "WORLDS_CACHE_TIME_MINUTES", default = 5)
 
     private inline fun <reified T> env(name: String, default: T? = null): T {
         //TODO: Allow enums here
